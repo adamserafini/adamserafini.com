64b49d0bc55d64e79b9caf11d6973e803967c121
adam <adam_serafini@yahoo.co.uk>
2016-07-31 21:37:14 +0200

    Fix typos.

--- js/fixstickyhover.js
+++ js/fixstickyhover.js
@@ -1,4 +1,4 @@
-// Unfortunately, mobiles, especially, iOS do weird stuff with :hover selector.
+// Unfortunately, mobiles, especially iOS, do weird stuff with :hover selector.
 // http://www.prowebdesign.ro/how-to-deal-with-hover-on-touch-screen-devices/
 
 if (!("ontouchstart" in document.documentElement)) {
