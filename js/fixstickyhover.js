aad9e0236d8d813a2cfae0b8de121cdb4c4f3ce6
adam <adam_serafini@yahoo.co.uk>
2016-07-31 20:53:09 +0200

    Second attempt to fix sticky hover by ignoring for touch.

--- js/fixstickyhover.js
+++ js/fixstickyhover.js
@@ -1,10 +1,6 @@
 // Unfortunately, mobiles, especially, iOS do weird stuff with :hover selector.
-// http://stackoverflow.com/q/17233804/996792
-function fix()
-{
-    var el = this;
-    var par = el.parentNode;
-    var next = el.nextSibling;
-    par.removeChild(el);
-    setTimeout(function() {par.insertBefore(el, next);}, 0)
+// http://www.prowebdesign.ro/how-to-deal-with-hover-on-touch-screen-devices/
+
+if (!("ontouchstart" in document.documentElement)) {
+document.documentElement.className += " no-touch";
 }
